3e7908ed0ccd40e9e1e5fc6cd7e5869ca6a74ea8
Aditya Raj <89244720+dhruvaditya@users.noreply.github.com>
2023-06-15 12:31:54 +0530

    Create input.c

---
+++ Compiler_Design_Lab_Assignment/input.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void){
+    printf("Hey this is the demo program");
+    //  This line will be removed from input file
+    // THis line will also be removed from input file
+    int a=10;
+    int b=230;
+    print(a+b);
+
+}
